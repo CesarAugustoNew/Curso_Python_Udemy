6f4010d36dad9e868dfe27da8660dad1f0ab1502
Cesar Augusto <cesarprofissional22@gmail.com>
2025-10-25 19:32:57 -0300

    Organizando pastas

---
+++ guppe/pep8.py
@@ -0,0 +1,42 @@
+"""
+PEP8 - PYTHON ENHANCEMENT PROPOSAL
+
+São propostas de melhoria para a linguagem P ython
+
+The Zen of Python
+import this
+A ideia da PEP8 é que possamos escrever códigos Python de forma Pythônica
+
+[1] - Utilize Camel Case para nomes de classes;
+
+class Calculadora:
+    pass
+
+class CalculadoraClientifica:
+    pass
+
+[2] - Utilize nomes em minúsculo, separados por underline para funções ou variaveis;
+
+def soma():
+    pass
+
+def soma_dois():
+    pass
+
+numero = 4
+numero_impar = 5
+
+[3] - Utilize 4 espaços para identação
+
+if 'a' in 'banana':
+    print('tem')
+
+[4] - Linhas em branco
+- Separar funçôes e definiçoes em classe com duas linhas em branco;
+- Metodos dentro de uma classe devem ser separados com uma única linha em branco;
+
+[5] - Imports
+
+- Imports devem ser sempre feitos em linha separadas;
+"""
+
---
+++ guppe/recebendo_dados_usiario.py
@@ -0,0 +1,17 @@
+"""
+Recebendo dados do usuário
+"""
+#print("Qual seu nome? ")
+#nome = input('Qual seu nome?')
+#nome = input()
+
+nome = input('Qual seu nome? ')
+
+#Exemplo de print mais atual
+print(f'Seja bem-vindo {nome}')
+
+idade = int(input('Qual sua idade? '))
+
+print(f'A {nome} tem {idade} anos')
+
+print(f'A {nome} nasceu em {2025 - idade}')
