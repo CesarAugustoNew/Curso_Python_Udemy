1aca411f7e1a6f4f45b19410090ae0199e29f648
Cesar Augusto <cesarprofissional22@gmail.com>
2025-10-25 20:48:58 -0300

    Tipos numerico, float, booleano

---
+++ guppe/tipo_booleano.py
@@ -0,0 +1,50 @@
+"""
+Tipo booleano
+
+Álgebra Booleana, criada por George Boole
+
+2 constantes, Verdadeiro ou False
+
+True -> Verdadeiro
+False -> Falso
+
+OBS: Sempre com a inicial maiúscula
+
+Errado:
+
+true, false
+
+Certo:
+
+True, False
+"""
+
+ativo = True
+print(ativo)
+
+"""
+Operações básicas:
+"""
+
+# Negação (not)
+"""
+Fazendo a negação, se o valor booleano for verdadeiro o resultado será falso,
+se for falso o resultado será verdadeiro. Ou seja, sempre o contrário.
+"""
+print(not ativo)
+
+logado = False
+
+# Ou (or)
+"""
+É uma operação binária, ou seja, depende de dois valores. Ou um ou outro deve ser verdadeiro.
+"""
+print(ativo or logado)
+
+# E (and)
+"""
+Também é uma operação binária, ou seja, depende de dois valores. Ambos os valores devem ser verdadeiros.
+"""
+num1 = 5
+num2 = 7
+valor = num2 >= num1
---
+++ guppe/tipo_float.py
@@ -0,0 +1,34 @@
+"""
+Tipo Float
+
+Tipo real, decimal
+
+Casas decimais
+
+OBS: O separador de casa decimais na programação é o ponto e não a vírgula
+"""
+#Errado do ponto de vista Float, mas gera um tupla
+valor = 1,44
+print(valor)
+
+#Certo do ponto de vista do Float
+valor = 1.44
+print(valor)
+
+#É possível fazer dupla atribuição
+valor1, valor2 = 1,44
+print(valor1)
+print(type(valor1))
+print(valor2)
+print(type(valor2))
+
+#Podemos converter um float para um int
+"""
+OBS: Ao converter valores float para inteiro nos podemos precisão
+"""
+res = int(valor)
+print(res)
+print(type(res))
+
+# Podemos trabalhar com números complexos
+variavel = 5j
