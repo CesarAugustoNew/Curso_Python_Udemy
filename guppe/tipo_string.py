cb24fc17f3e842fc0a82233be1eb3a1ce8515ffb
Cesar Augusto <cesarprofissional22@gmail.com>
2025-10-25 22:32:54 -0300

    Tipo string

---
+++ guppe/tipo_string.py
@@ -0,0 +1,23 @@
+"""
+Tipo string
+
+Em Python, um dado é considerado do tipo string sempre que:
+
+- Estiver entre áspas simples, duplas ou triplas -> 'uma string', '123', 'True' '42.s'
+
+
+print(nome.upper())
+
+print(nome.lower())
+
+print(nome.slit()) # Transforma em uma lista de strings
+
+print(nome[0:4]) #Slice de string
+"""
+nome = 'Geek University'
+
+print(nome[::-1]) #Inversão da String
+
+print(nome.replace('G', 'P'))
+
+print(type(nome))
