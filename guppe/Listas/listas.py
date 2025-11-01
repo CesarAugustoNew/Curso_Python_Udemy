295d7aadfcf9036fc46bdaa5004e349f0f47da49
Cesar Augusto <cesarprofissional22@gmail.com>
2025-10-31 22:40:45 -0300

    commit

--- guppe/Listas/listas.py
+++ guppe/Listas/listas.py
@@ -51,3 +51,30 @@ else:
 
 lista1.extend([123,34,12]) # Coloca cada elemento como valor adicional a lista
 print(lista1)
+
+mensagem = ("iniciando")
+print(mensagem)
+
+# Adicionar elementos em listas
+
+"""
+Para adicionar elementos em listas, utilizamos a função append
+
+OBS: Com append, nos so conseguimos adicionar 1 elemento por vez
+"""
+print(lista1)
+lista1.append(100)
+print(lista1)
+
+lista1.append([9,3,1])  # Coloca a lista como elemento unico sublistas
+print(lista1)
+if [9,3,1] in lista1:
+    print(f"Encontrei a lista")
+else:
+    print(f"Nao encontrei a lista")
+
+lista1.extend([123,34,12]) # Coloca cada elemento como valor adicional a lista
+print(lista1)
+
+mensagem = ("iniciando")
+print(mensagem)
